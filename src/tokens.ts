df1ce3791d19edfb777ab18c5f3045cd9840383e
Nikos Kapraras <nikos@kapraran.dev>
2021-09-30 01:36:14 +0200

    add TokenName enum

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,4 +1,5 @@
 import { createToken, TokenType, Lexer, ITokenConfig } from 'chevrotain'
+import { TokenName } from './TokenName'
 
 export const tokenMap: Record<string, TokenType> = {}
 
@@ -11,274 +12,274 @@ function _addToken(config: ITokenConfig): void {
 }
 
 _addToken({
-  name: 'Comment',
+  name: TokenName.Comment,
   pattern: /\!.*(\r\n|\r|\n)/,
   group: Lexer.SKIPPED,
 })
 
 _addToken({
-  name: 'WhiteSpace',
+  name: TokenName.WhiteSpace,
   pattern: /\s+/,
   group: Lexer.SKIPPED,
 })
 
 _addToken({
-  name: 'StringVal',
+  name: TokenName.StringVal,
   pattern:
     /("(:?[^\\"\n\r]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"|'(:?[^\\'\n\r]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*')/,
 })
 
 _addToken({
-  name: 'RealVal',
+  name: TokenName.RealVal,
   pattern: /(0|[1-9]\d*)(\.\d+)([eE][+-]?\d+)?/,
 })
 
 _addToken({
-  name: 'IntegerVal',
+  name: TokenName.IntegerVal,
   pattern: /(0|[1-9]\d*)/,
   longer_alt: tokenMap.RealVal,
 })
 
-_addToken({ name: 'BooleanVal', pattern: /ΑΛΗΘΗΣ|ΨΕΥΔΗΣ/ })
+_addToken({ name: TokenName.BooleanVal, pattern: /ΑΛΗΘΗΣ|ΨΕΥΔΗΣ/ })
 
 // keywords
 _addToken({
-  name: 'Program',
+  name: TokenName.Program,
   pattern: /ΠΡΟΓΡΑΜΜΑ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'End',
+  name: TokenName.End,
   pattern: /ΤΕΛΟΣ_ΠΡΟΓΡΑΜΜΑΤΟΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Variables',
+  name: TokenName.Variables,
   pattern: /ΜΕΤΑΒΛΗΤΕΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'DeclTypeSpecifier',
+  name: TokenName.DeclTypeSpecifier,
   pattern: /ΑΚΕΡΑΙΕΣ|ΠΡΑΓΜΑΤΙΚΕΣ|ΧΑΡΑΚΤΗΡΕΣ|ΛΟΓΙΚΕΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'DeclRetTypeSpecifier',
+  name: TokenName.DeclRetTypeSpecifier,
   pattern: /ΑΚΕΡΑΙΑ|ΠΡΑΓΜΑΤΙΚΗ|ΧΑΡΑΚΤΗΡΑΣ|ΛΟΓΙΚΗ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Constants',
+  name: TokenName.Constants,
   pattern: /ΣΤΑΘΕΡΕΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Read',
+  name: TokenName.Read,
   pattern: /ΔΙΑΒΑΣΕ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Write',
+  name: TokenName.Write,
   pattern: /ΓΡΑΨΕ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Not',
+  name: TokenName.Not,
   pattern: /ΟΧΙ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'And',
+  name: TokenName.And,
   pattern: /ΚΑΙ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Or',
+  name: TokenName.Or,
   pattern: /Ή/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'If',
+  name: TokenName.If,
   pattern: /ΑΝ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Then',
+  name: TokenName.Then,
   pattern: /ΤΟΤΕ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'ElseIf',
+  name: TokenName.ElseIf,
   pattern: /ΑΛΛΙΩΣ_ΑΝ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Else',
+  name: TokenName.Else,
   pattern: /ΑΛΛΙΩΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'EndIf',
+  name: TokenName.EndIf,
   pattern: /ΤΕΛΟΣ_ΑΝ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Select',
+  name: TokenName.Select,
   pattern: /ΕΠΙΛΕΞΕ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Case',
+  name: TokenName.Case,
   pattern: /ΠΕΡΙΠΤΩΣΗ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'EndSelect',
+  name: TokenName.EndSelect,
   pattern: /ΤΕΛΟΣ_ΕΠΙΛΟΓΩΝ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'For',
+  name: TokenName.For,
   pattern: /ΓΙΑ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'From',
+  name: TokenName.From,
   pattern: /ΑΠΟ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'While',
+  name: TokenName.While,
   pattern: /ΟΣΟ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Repeat',
+  name: TokenName.Repeat,
   pattern: /ΕΠΑΝΑΛΑΒΕ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'StartRepeat',
+  name: TokenName.StartRepeat,
   pattern: /ΑΡΧΗ_ΕΠΑΝΑΛΗΨΗΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Start',
+  name: TokenName.Start,
   pattern: /ΑΡΧΗ/,
   longer_alt: tokenMap.StartRepeat,
 })
 
 _addToken({
-  name: 'DoUntil',
+  name: TokenName.DoUntil,
   pattern: /ΜΕΧΡΙΣ_ΟΤΟΥ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Until',
+  name: TokenName.Until,
   pattern: /ΜΕΧΡΙ/,
   longer_alt: tokenMap.DoUntil,
 })
 
 _addToken({
-  name: 'EndRepeat',
+  name: TokenName.EndRepeat,
   pattern: /ΤΕΛΟΣ_ΕΠΑΝΑΛΗΨΗΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Step',
+  name: TokenName.Step,
   pattern: /ΜΕ\s+ΒΗΜΑ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Procedure',
+  name: TokenName.Procedure,
   pattern: /ΔΙΑΔΙΚΑΣΙΑ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'EndProcedure',
+  name: TokenName.EndProcedure,
   pattern: /ΤΕΛΟΣ_ΔΙΑΔΙΚΑΣΙΑΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Function',
+  name: TokenName.Function,
   pattern: /ΣΥΝΑΡΤΗΣΗ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'EndFunc',
+  name: TokenName.EndFunc,
   pattern: /ΤΕΛΟΣ_ΣΥΝΑΡΤΗΣΗΣ/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Call',
+  name: TokenName.Call,
   pattern: /ΚΑΛΕΣΕ/,
   longer_alt: tokenMap.Identifier,
 })
 
-_addToken({ name: 'Colon', pattern: /:/ })
-_addToken({ name: 'Comma', pattern: /,/ })
-_addToken({ name: 'LSquare', pattern: /\[/ })
-_addToken({ name: 'RSquare', pattern: /]/ })
-_addToken({ name: 'LParen', pattern: /\(/ })
-_addToken({ name: 'RParen', pattern: /\)/ })
-_addToken({ name: 'Equal', pattern: /=/ })
-_addToken({ name: 'AssignOp', pattern: /<\-\-/ })
-_addToken({ name: 'DbDots', pattern: /\.\./ })
+_addToken({ name: TokenName.Colon, pattern: /:/ })
+_addToken({ name: TokenName.Comma, pattern: /,/ })
+_addToken({ name: TokenName.LSquare, pattern: /\[/ })
+_addToken({ name: TokenName.RSquare, pattern: /]/ })
+_addToken({ name: TokenName.LParen, pattern: /\(/ })
+_addToken({ name: TokenName.RParen, pattern: /\)/ })
+_addToken({ name: TokenName.Equal, pattern: /=/ })
+_addToken({ name: TokenName.AssignOp, pattern: /<\-\-/ })
+_addToken({ name: TokenName.DbDots, pattern: /\.\./ })
 
 _addToken({
-  name: 'RelOp',
+  name: TokenName.RelOp,
   pattern: /(<>|<=|>=|<|>)/,
 })
 
 _addToken({
-  name: 'Plus',
+  name: TokenName.Plus,
   pattern: /\+/,
 })
 
 _addToken({
-  name: 'Minus',
+  name: TokenName.Minus,
   pattern: /\-/,
 })
 
 _addToken({
-  name: 'MulOp',
+  name: TokenName.MulOp,
   pattern: /(\*|\/|\^|DIV|MOD)/,
   longer_alt: tokenMap.Identifier,
 })
 
 _addToken({
-  name: 'Identifier',
+  name: TokenName.Identifier,
   pattern:
     /[_a-zA-Z\u0370-\u03ff\u1f00-\u1fff]([_a-zA-Z\u0370-\u03ff\u1f00-\u1fff]|\d)*/,
 })
